14a743bb01d38a8069ee497ba609d986c9db1ca1
nyaago <nyaago69@gmail.com>
2015-07-12 14:52:34 +0900

    Initial Commit

---
+++ IOSSpeech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSSpeech
+//
+//  Created by nyaago on 2015/07/12.
+//  Copyright (c) 2015年 nyaago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IOSSpeechTests/IOSSpeechTests.swift
@@ -0,0 +1,36 @@
+//
+//  IOSSpeechTests.swift
+//  IOSSpeechTests
+//
+//  Created by nyaago on 2015/07/12.
+//  Copyright (c) 2015年 nyaago. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class IOSSpeechTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
